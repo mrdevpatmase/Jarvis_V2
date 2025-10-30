65cc2c81c622dbeec2c0e6b9ed462570d96fb8a9
Dev Patmase <devpatmase1@gmail.com>
2025-10-30 15:38:28 +0530

    Update for deployment

--- app.py
+++ app.py
@@ -9,6 +9,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# @app.route("/give_command", methods=['POST'])
+# def give_command():
+#     data = request.get_json()
+#     command = data.get("command", "").strip()
+
+#     if not command:
+#         return jsonify({"status": "No command received."})
+
+#     print(f"🎤 Command received: {command}")
+#     intent = find_intent(command)
+#     execute_intent(intent)
+#     return jsonify({"status": f"Executed intent: {intent}"})
+
+
 @app.route("/give_command", methods=['POST'])
 def give_command():
     data = request.get_json()
@@ -19,8 +33,10 @@ def give_command():
 
     print(f"🎤 Command received: {command}")
     intent = find_intent(command)
-    execute_intent(intent)
-    return jsonify({"status": f"Executed intent: {intent}"})
+    response_text = execute_intent(intent)
+
+    return jsonify({"status": response_text})
+
 
 
 @app.route("/stop_command", methods=['POST'])
